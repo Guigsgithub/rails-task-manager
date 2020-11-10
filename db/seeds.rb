d1077b4b5706fa7e02d6cb9b96485b7093c2c574
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-10 13:11:38 +0100

    index, show, link and back link created

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create title: 'Laundry', details: 'Do not mix colors!'
+Task.create title: 'Studying', details: 'A lot of flashcards to do', completed: true
